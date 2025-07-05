aaf8c8e70769e135415c1e97864afa23a0499316
Anand6262 <pyexpert62@gmail.com>
2025-07-05 17:58:52 +0530

    chanegs

--- src/views/Child.jsx
+++ src/views/Child.jsx
@@ -3,14 +3,15 @@ import React from 'react'
 function Child(props) {
 
     console.log(props);
-    
 
-  return (
-    <>
-        <input type="text" onChange={(e) => props.setName(e.target.value)} />
-        <p>Name state variable ki value inside card(parent to child):: {props.name}</p>
-    </>
-  )
+
+    return (
+        <>
+            {/* We will change state here & it will reflect on parent cmponent. */}
+            <input type="text" onChange={(e) => props.setName(e.target.value)} />
+            <p>Name state variable ki value inside card(parent to child):: {props.name}</p>
+        </>
+    )
 }
 
 export default Child
